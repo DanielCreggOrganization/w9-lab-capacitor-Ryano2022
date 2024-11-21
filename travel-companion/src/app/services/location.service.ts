b5d3b0692eff3a58e027d0ba158366257fdeb447
Ryan Hogan <101425132+Ryano2022@users.noreply.github.com>
2024-11-21 11:19:21 +0000

    Add camera functionality and location service implementation.

---
+++ travel-companion/src/app/services/location.service.ts
@@ -0,0 +1,16 @@
+// src/app/services/location.service.ts
+import { Injectable } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocationService {
+  constructor() { }
+
+  async getCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+
+    console.log("Current position: ", coordinates);
+  }
+}
